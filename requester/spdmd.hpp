ebd9a544418ed73c6fdad9db6c383b77c5c2e43d
Ratan Gupta <ratankgupta31@gmail.com>
2025-04-15 10:06:23 +0530

    spdmd: Establish basic daemon structure
    Key Components:
    - spdmd_app: Main daemon class with async D-Bus context
    - spdmd: Entry point with exception handling and logging
    - meson.build: Build configuration with C++23 and dependencies
    
    Build System:
    - Configure meson with phosphor-logging and sdbusplus dependencies
    - Set up subproject dependencies via wrap files
    - Enable code quality tools (clang-format, clang-tidy)
    - Use C++23 standard with warning level 3
    
    This establishes the basic daemon structure that will be extended with
    SPDM discovery and communication functionality in future commits.
    
    Change-Id: Ied4ca213226a46546f3544723d39ff4a4596d213
    Signed-off-by: Ratan Gupta <ratankgupta31@gmail.com>

---
+++ requester/spdmd.hpp
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: Copyright OpenBMC Authors
+
+#include <xyz/openbmc_project/Attestation/ComponentIntegrity/common.hpp>
+
+constexpr const char* objManagerPath =
+    sdbusplus::common::xyz::openbmc_project::attestation::namespace_path;
+constexpr const char* dbusServiceName = "xyz.openbmc_project.spdm.requester";
