e3c96f0ad3e60daa44833746f6169171a01c2261
Ratan Gupta <ratankgupta31@gmail.com>
2025-04-15 10:06:23 +0530

    spdmd: Add initial daemon structure
    Initial implementation of the SPDM daemon with basic D-Bus setup.
    This commit establishes the foundational structure including:
    - Basic daemon initialization
    - D-Bus object manager setup
    - Namespace organization
    - Basic error handling
    
    The daemon will be expanded in future commits to add SPDM-specific
    functionality and more robust error handling.
    
    Change-Id: Ied4ca213226a46546f3544723d39ff4a4596d213
    Signed-off-by: Ratan Gupta <ratankgupta31@gmail.com>

---
+++ src/spdmd_app.hpp
@@ -0,0 +1,53 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2022-2024 NVIDIA CORPORATION &
+ * AFFILIATES. All rights reserved. SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <sdbusplus/asio/connection.hpp>
+#include <sdbusplus/async.hpp>
+#include <sdbusplus/server/manager.hpp>
+
+namespace spdmd
+{
+
+class SPDMDaemon
+{
+  public:
+    SPDMDaemon();
+    int run();
+
+    /** @brief Get the D-Bus connection */
+    sdbusplus::bus::bus& getBus()
+    {
+        return ctx.get_bus();
+    }
+
+    /** @brief Get the async context for parallel coroutine execution */
+    sdbusplus::async::context& getAsyncContext()
+    {
+        return ctx;
+    }
+
+  private:
+    sdbusplus::async::context ctx;
+    sdbusplus::server::manager_t objManager;
+
+    static constexpr auto spdmRootPath = "/xyz/openbmc_project/SPDM";
+    static constexpr auto spdmBusName = "xyz.openbmc_project.SPDM";
+};
+
+} // namespace spdmd
