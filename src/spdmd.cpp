1fa3218b9a52ce52b68397d3c392bd20e64a813a
Ratan Gupta <guptar@nvidia.com>
2025-05-06 15:50:50 +0530

    spdmd: Add D-Bus responder for SPDM devices
    - Add SPDMDBusResponder class for D-Bus interface management
    - Add responder integration with device discovery
    
    Change-Id: I1c6259658be6d76f8fafb4a465f271402dfa4b10
    Signed-off-by: Ratan Gupta <ratankgupta31@gmail.com>

--- src/spdmd.cpp
+++ src/spdmd.cpp
@@ -2,6 +2,7 @@
 // SPDX-FileCopyrightText: Copyright OpenBMC Authors
 
 #include "mctp_transport_discovery.hpp"
+#include "spdm_dbus_responder.hpp"
 #include "spdm_discovery.hpp"
 #include "spdmd_app.hpp"
 
@@ -26,16 +27,32 @@ int main()
         // Assign the discovery protocol to the discovery object - Context
         spdm::SPDMDiscovery discovery(std::move(discoveryProtocol));
 
+        std::vector<std::unique_ptr<spdm::SPDMDBusResponder>> responders;
         // Perform discovery
         if (discovery.discover())
         {
             // Print discovered devices
             for (const auto& device : discovery.respInfos)
             {
-                std::cout << "Found SPDM device:\n"
-                          << "  Path: " << device.objectPath << "\n"
-                          << "  EID: " << device.eid << "\n"
-                          << "  UUID: " << device.uuid << "\n";
+                try
+                {
+                    lg2::info("Creating D-Bus responder for device {PATH}",
+                              "PATH", device.objectPath);
+                    // Create SPDMDBusResponder with ResponderInfo and async
+                    // context for parallel execution
+                    auto responder = std::make_unique<spdm::SPDMDBusResponder>(
+                        bus, device, app.ctx);
+                    responders.push_back(std::move(responder));
+                    lg2::info(
+                        "Successfully created responder for device {PATH}",
+                        "PATH", device.objectPath);
+                }
+                catch (...)
+                {
+                    lg2::error("Unknown error processing device {PATH}", "PATH",
+                               device.objectPath);
+                    continue;
+                }
             }
         }
         else
