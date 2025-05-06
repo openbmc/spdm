8eebf10df5042dfa28a9972e29b43f91a0b91161
Ratan Gupta <ratankgupta31@gmail.com>
2025-05-06 16:13:57 +0530

    spdmd: Add D-Bus component integrity interface
    Add D-Bus interface implementation for SPDM component integrity.
    This enables exposing SPDM device measurements, authentication,
    and integrity verification through standardized D-Bus interfaces.
    
    - Add ComponentIntegrity class implementing:
      * xyz.openbmc_project.Attestation.ComponentIntegrity
      * xyz.openbmc_project.Attestation.IdentityAuthentication
      * xyz.openbmc_project.Attestation.MeasurementSet
    
    Testing:
    - Verified D-Bus interface registration
    
    Change-Id: Idf66f74bf21c5289f4889dca12cacc466336e84e
    Signed-off-by: Ratan Gupta <ratankgupta31@gmail.com>

--- requester/spdm_dbus_responder.cpp
+++ requester/spdm_dbus_responder.cpp
@@ -12,11 +12,15 @@ PHOSPHOR_LOG2_USING;
 namespace spdm
 {
 
-SPDMDBusResponder::SPDMDBusResponder(const ResponderInfo& responderInfo) :
+SPDMDBusResponder::SPDMDBusResponder(sdbusplus::async::context& ctx,
+                                     const ResponderInfo& responderInfo) :
     responder(responderInfo)
 {
     const auto devName = name();
-
+    std::string componentIntegrityPath =
+        "/xyz/openbmc_project/ComponentIntegrity/" + devName;
+    componentIntegrity =
+        std::make_unique<ComponentIntegrity>(ctx, componentIntegrityPath);
     info("Created SPDM D-Bus responder for device {ID} at {PATH}", "ID",
          devName, "PATH", responder.path);
 }
