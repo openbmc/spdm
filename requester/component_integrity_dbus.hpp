4b450f95fc2618c1aca844072c2397f1d910c56f
Ratan Gupta <ratankgupta31@gmail.com>
2025-07-28 23:17:38 +0530

    spdmd: Implement Digest
    The digest functionality enables retrieval of
    certificate digests from SPDM devices,
    Supporting the ComponentIntegrity D-Bus interface
    for attestation and measurement verification workflows.
    
    Signed-off-by: Ratan Gupta <ratankgupta31@gmail.com>
    
    Change-Id: Iafd0eb84cb4427cb19a6d6fd58b3e77e0af1f711
    Signed-off-by: Ratan Gupta <ratankgupta31@gmail.com>

--- requester/component_integrity_dbus.hpp
+++ requester/component_integrity_dbus.hpp
@@ -19,6 +19,12 @@
 
 namespace spdm
 {
+class SpdmTransport;
+
+namespace test
+{
+class ComponentIntegrityTest;
+}
 
 /** @class ComponentIntegrity
  *  @brief OpenBMC ComponentIntegrity entry implementation.
@@ -66,6 +72,15 @@ class ComponentIntegrity :
     ~ComponentIntegrity() = default;
 
     /**
+     * @brief Set the SPDM transport reference
+     * @param transportIn SPDM transport instance
+     */
+    void setTransport(std::shared_ptr<spdm::SpdmTransport> transportIn)
+    {
+        transport = transportIn;
+    }
+
+    /**
      * @brief Request stop of the async context
      */
     void stopAsyncContext()
@@ -82,7 +97,6 @@ class ComponentIntegrity :
                          std::chrono::system_clock::now().time_since_epoch())
                          .count());
     }
-
     /**
      * @brief Get signed measurements from SPDM device
      * @param measurementIndices Array of measurement indices to sign
@@ -108,6 +122,30 @@ class ComponentIntegrity :
     /** @brief Object path for this component */
     std::string path;
 
+  protected:
+    /**
+     * @brief Validate measurement indices
+     * @param measurementIndices Vector of measurement indices to validate
+     * @throws InvalidArgument if any index is invalid
+     */
+    void validateMeasurementIndices(
+        const std::vector<size_t>& measurementIndices);
+
+    /**
+     * @brief Initialize SPDM connection
+     * @throws std::runtime_error if initialization fails
+     */
+    void initializeSpdmConnection();
+
+    /**
+     * @brief Get certificate digests from SPDM device
+     * @return Tuple of (slotMask, digestBuffer, totalDigestSize)
+     */
+    std::tuple<uint8_t, std::vector<uint8_t>, size_t> getCertificateDigests();
+
+  private:
+    std::shared_ptr<spdm::SpdmTransport> transport;
+
     /** @brief Async context for D-Bus operations */
     sdbusplus::async::context& asyncCtx;
 
@@ -117,6 +155,9 @@ class ComponentIntegrity :
      * properties
      */
     void initializeProperties();
+
+    // Friend declaration for testing
+    friend class spdm::test::ComponentIntegrityTest;
 };
 
 } // namespace spdm
