e57df87fa838256e05524aee63da961009f0bc08
Archana Kakani <archana.kakani@ibm.com>
2026-02-25 00:29:32 -0600

    spdmd: Add initial discovery of SPDM TCP responder
    Add support for discovering SPDM TCP responders by querying the
    ObjectMapper for objects implementing the
    xyz.openbmc_project.Configuration.SpdmTcpResponder interface.
    
    The SPDM daemon retrieves the remote TCP endpoint configuration,
    including the hostname/IP address and port, from the discovered
    SpdmTcpResponder objects.
    
    Change-Id: Id672d948db4e4139be1f6c282a06a385d6136265
    Signed-off-by: Archana Kakani <archana.kakani@ibm.com>
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>

--- requester/tcp_transport_discovery.cpp
+++ requester/tcp_transport_discovery.cpp
@@ -3,23 +3,44 @@
 
 #include "tcp_transport_discovery.hpp"
 
+#include "utils/mapper.hpp"
+
 #include <phosphor-logging/lg2.hpp>
+#include <sdbusplus/async/client.hpp>
+#include <xyz/openbmc_project/Configuration/SpdmTcpResponder/client.hpp>
+#include <xyz/openbmc_project/ObjectMapper/client.hpp>
 
 #include <algorithm>
 
 namespace spdm
 {
+PHOSPHOR_LOG2_USING;
 
-auto TCPTransportDiscovery::discovery(SPDMDiscovery&)
+auto TCPTransportDiscovery::discovery(SPDMDiscovery& discovery)
     -> sdbusplus::async::task<>
 {
-    using namespace std::literals;
-    PHOSPHOR_LOG2_USING;
+    using Configuration = sdbusplus::client::xyz::openbmc_project::
+        configuration::SpdmTcpResponder<>;
 
-    // TODO: Add real discovery, for now just adding pause
-    co_await sdbusplus::async::sleep_for(ctx, 1s);
+    auto instances =
+        co_await mapper::instances::by_interface<Configuration>(ctx);
 
-    debug("TCPTransportDiscovery: discovery complete");
+    for (const auto& [path, service] : instances)
+    {
+        auto properties = co_await Configuration(ctx)
+                              .service(service)
+                              .path(path.str)
+                              .properties();
+
+        debug("Found SPDM TCP Responder at {IP}:{PORT} for {PATH}", "IP",
+              properties.hostname, "PORT", properties.port, "PATH", path);
+
+        discovery.add(ResponderInfo{
+            path, TcpResponderInfo{properties.hostname, properties.port},
+            TransportType::TCP});
+    }
+
+    debug("TCP transport discovery completed");
 }
 
 } // namespace spdm
