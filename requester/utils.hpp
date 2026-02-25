c2d5a737917fc0157076f01a34d354a42d9cc049
Archana Kakani <archana.kakani@ibm.com>
2026-02-25 00:29:32 -0600

    spdmd: Add SPDM TCP responder discovery via EM
    Add support for discovering SPDM TCP responders through
    Entity-Manager.
    
    The SPDM daemon queries Entity-Manager for remote TCP endpoint
    configuration and retrieves the hostname/IP address and port
    from the SpdmTcpResponder interface.
    
    Change-Id: Id672d948db4e4139be1f6c282a06a385d6136265
    Signed-off-by: Archana Kakani <archana.kakani@ibm.com>

---
+++ requester/utils.hpp
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: Copyright OpenBMC Authors
+
+#pragma once
+
+#include <sdbusplus/async.hpp>
+#include <sdbusplus/message.hpp>
+
+#include <functional>
+#include <map>
+#include <string>
+#include <variant>
+
+namespace spdm
+{
+
+// Type aliases for complex D-Bus structures
+using DbusPropertyValue =
+    std::variant<std::string, uint8_t, std::vector<uint8_t>, uint64_t>;
+using DbusInterface = std::map<std::string, DbusPropertyValue>;
+using DbusInterfaces = std::map<std::string, DbusInterface>;
+using ManagedObjects =
+    std::map<sdbusplus::message::object_path, DbusInterfaces>;
+using SubTreeType =
+    std::map<std::string, std::map<std::string, std::vector<std::string>>>;
+
+/**
+ * @brief Get objects from ObjectMapper that implement a specific interface
+ * @param ctx Async context
+ * @param interface Interface name to search for
+ * @param serviceName Optional service name to filter results
+ * @return SubTree of objects implementing the interface
+ */
+auto getObjectsFromMapper(sdbusplus::async::context& ctx,
+                          const std::string& interface,
+                          const std::string& serviceName = "")
+    -> sdbusplus::async::task<SubTreeType>;
+
+/**
+ * @brief Asynchronously retrieves a D-Bus property value
+ *
+ * @tparam T Property value type
+ * @param ctx Async context for the operation
+ * @param service D-Bus service name
+ * @param objectPath D-Bus object path
+ * @param interface D-Bus interface name
+ * @param property Property name to retrieve
+ * @return property value of type T
+ */
+template <typename T>
+inline auto getProperty(
+    sdbusplus::async::context& ctx, const std::string& service,
+    const std::string& objectPath, const std::string& interface,
+    const std::string& property) -> sdbusplus::async::task<T>
+{
+    auto proxy = sdbusplus::async::proxy()
+                     .service(service)
+                     .path(objectPath)
+                     .interface(interface);
+
+    co_return co_await proxy.get_property<T>(ctx, property);
+}
+
+} // namespace spdm
