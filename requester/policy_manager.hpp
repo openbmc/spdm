aa5df8bb96cd402988919f1201514063ba74ce84
Manjeet Singh <itsmanjeet1998@gmail.com>
2026-03-09 12:31:05 +0530

    Implement SPDM Policy D-Bus interface
    Add support for the `xyz.openbmc_project.Control.Security.SPDM.Policy`
    D-Bus interface, enabling a configurable framework for managing
    platform SPDM security policies.
    
    Expose properties to control SPDM policy behavior as defined in
    the PDI specification [1].
    
    [1]: http://github.com/openbmc/phosphor-dbus-interfaces/commit/0d84eaf4ea2ef3ac3eb09b6ac2d2b85fbebf9e85
    
    Change-Id: I88bb1a0c825c7ef893eaac4ad6da3cd8545bbd2a
    Signed-off-by: Manjeet Singh <itsmanjeet1998@gmail.com>

---
+++ requester/policy_manager.hpp
@@ -0,0 +1,71 @@
+#pragma once
+
+#include <nlohmann/json.hpp>
+#include <phosphor-logging/lg2.hpp>
+#include <xyz/openbmc_project/Control/Security/SPDM/Policy/aserver.hpp>
+#include <xyz/openbmc_project/Control/Security/SPDM/Policy/common.hpp>
+
+#include <exception>
+#include <expected>
+#include <utility>
+
+class PolicyManager :
+    public sdbusplus::aserver::xyz::openbmc_project::control::security::spdm::
+        Policy<PolicyManager>
+{
+  public:
+    explicit PolicyManager(sdbusplus::async::context& ctx, auto path) :
+        sdbusplus::aserver::xyz::openbmc_project::control::security::spdm::
+            Policy<PolicyManager>(ctx, path)
+    {
+        PHOSPHOR_LOG2_USING;
+
+        if (auto result = load(); !result)
+        {
+            warning("failed to load cached policy, falling back to default");
+        }
+    }
+
+    auto dump() -> std::expected<void, std::exception_ptr>;
+
+    auto load() -> std::expected<void, std::exception_ptr>;
+
+    auto set_property(enabled_t, auto enabled) -> bool
+    {
+        return update_property(enabled_, enabled);
+    }
+
+    auto set_property(secure_session_enabled_t, auto secure_session_enabled)
+        -> bool
+    {
+        return update_property(secure_session_enabled_, secure_session_enabled);
+    }
+
+  private:
+    template <typename T, typename U, typename F = std::nullptr_t>
+        requires std::assignable_from<T&, U> && std::equality_comparable<T> &&
+                 (std::same_as<F, std::nullptr_t> ||
+                  std::invocable<F, const T&>)
+    auto update_property(T& current, U&& value, F&& f = nullptr) -> bool
+    {
+        PHOSPHOR_LOG2_USING;
+
+        if (current == value)
+        {
+            return false;
+        }
+
+        current = std::forward<U>(value);
+
+        if constexpr (!std::same_as<F, std::nullptr_t>)
+        {
+            std::invoke(std::forward<F>(f), std::as_const(current));
+        }
+
+        if (auto result = dump(); !result)
+        {
+            warning("failed to dump policy cache");
+        }
+        return true;
+    }
+};
