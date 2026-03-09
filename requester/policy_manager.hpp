9317ab04a5ce9de0ebe5fa182d2b62e51a00848e
Manjeet Singh <itsmanjeet1998@gmail.com>
2026-03-09 12:31:05 +0530

    Implement SPDM Policy D-Bus interface
    Add support for the `xyz.openbmc_project.Control.Security.SPDM.Policy`
    D-Bus interface, enabling a configurable framework for managing
    platform SPDM security policies.
    
    Expose properties to control SPDM policy behavior as defined in
    the PDI specification [1].
    
    [1]: http://github.com/openbmc/phosphor-dbus-interfaces/commit/0d84eaf4ea2ef3ac3eb09b6ac2d2b85fbebf9e85
    
    Change-Id: I88bb1a0c825c7ef893eaac4ad6da3cd8545bbd2a
    Signed-off-by: Manjeet Singh <itsmanjeet1998@gmail.com>

---
+++ requester/policy_manager.hpp
@@ -0,0 +1,131 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: Copyright OpenBMC Authors
+
+#pragma once
+
+#include <nlohmann/json.hpp>
+#include <phosphor-logging/lg2.hpp>
+#include <xyz/openbmc_project/Control/Security/SPDM/Policy/aserver.hpp>
+
+#include <filesystem>
+#include <functional>
+#include <utility>
+
+class PolicyManager :
+    public sdbusplus::aserver::xyz::openbmc_project::control::security::spdm::
+        Policy<PolicyManager>
+{
+  public:
+    explicit PolicyManager(sdbusplus::async::context& ctx, auto path,
+                           std::filesystem::path cache_path = {}) :
+        sdbusplus::aserver::xyz::openbmc_project::control::security::spdm::
+            Policy<PolicyManager>(ctx, path),
+        cache_path_(std::move(cache_path))
+    {
+        PHOSPHOR_LOG2_USING;
+
+        load();
+    }
+
+    virtual ~PolicyManager() = default;
+
+    template <typename T>
+    auto set_property(T, auto value) -> bool
+    {
+        if constexpr (std::is_same_v<T, enabled_t>)
+        {
+            return update_property(enabled_, value, enabled_callback_);
+        }
+        else if constexpr (std::is_same_v<T, secure_session_enabled_t>)
+        {
+            return update_property(secure_session_enabled_, value,
+                                   secure_session_enabled_callback_);
+        }
+        else if constexpr (std::is_same_v<T, verify_certificate_t>)
+        {
+            return update_property(verify_certificate_, value,
+                                   verify_certificate_callback_);
+        }
+        else if constexpr (std::is_same_v<T, allow_extended_algorithms_t>)
+        {
+            return update_property(allow_extended_algorithms_, value,
+                                   allow_extended_algorithms_callback_);
+        }
+        else if constexpr (std::is_same_v<T, allowed_versions_t>)
+        {
+            return update_property(allowed_versions_, value);
+        }
+        else if constexpr (std::is_same_v<T, allowed_algorithms_aead_t>)
+        {
+            return update_property(allowed_algorithms_aead_, value);
+        }
+        else if constexpr (std::is_same_v<T, allowed_algorithms_base_hash_t>)
+        {
+            return update_property(allowed_algorithms_base_hash_, value);
+        }
+        else if constexpr (std::is_same_v<T, allowed_algorithms_base_asym_t>)
+        {
+            return update_property(allowed_algorithms_base_asym_, value);
+        }
+    }
+
+    template <typename F>
+        requires std::invocable<F, bool>
+    auto on_enabled(F&& callback) -> void
+    {
+        enabled_callback_ = std::forward<F>(callback);
+    }
+
+    template <typename F>
+        requires std::invocable<F, bool>
+    auto on_secure_session_enabled(F&& callback) -> void
+    {
+        secure_session_enabled_callback_ = std::forward<F>(callback);
+    }
+
+    template <typename F>
+        requires std::invocable<F, bool>
+    auto on_verify_certificate(F&& callback) -> void
+    {
+        verify_certificate_callback_ = std::forward<F>(callback);
+    }
+
+  private:
+    template <typename T, typename U, typename F = std::nullptr_t>
+        requires std::assignable_from<T&, U> && std::equality_comparable<T> &&
+                 (std::same_as<F, std::nullptr_t> ||
+                  std::invocable<F, const T&>)
+    auto update_property(T& current, U&& value, F&& f = nullptr) -> bool
+    {
+        PHOSPHOR_LOG2_USING;
+
+        if (current == value)
+        {
+            return false;
+        }
+
+        current = std::forward<U>(value);
+
+        if constexpr (!std::same_as<F, std::nullptr_t>)
+        {
+            std::invoke(std::forward<F>(f), std::as_const(current));
+        }
+
+        dump();
+        return true;
+    }
+
+    auto dump() -> void;
+
+    auto load() -> void;
+
+    auto unmarshal_config(const nlohmann::json& config) -> void;
+
+    auto marshal_config() -> nlohmann::json;
+
+    std::function<void(bool)> enabled_callback_{nullptr};
+    std::function<void(bool)> secure_session_enabled_callback_{nullptr};
+    std::function<void(bool)> verify_certificate_callback_{nullptr};
+    std::function<void(bool)> allow_extended_algorithms_callback_{nullptr};
+    std::filesystem::path cache_path_;
+};
