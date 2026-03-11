b0df5c0f19cde8a49c73e5abb3170debf92044b2
Archana Kakani <archana.kakani@ibm.com>
2026-03-11 01:25:19 -0500

    spdm_discovery: Add method to remove discovered responder
    Introduce a remove() method in the SPDMDiscovery class that enables
    removal of SPDM responder devices by their D-Bus object path.
    
    The implementation locates the responder matching the provided object
    path and erases it from the internal responderInfos collection.
    
    Change-Id: I370b38f72b35a6c6e21aead2b72bae30b49146c1
    Signed-off-by: Archana Kakani <archana.kakani@ibm.com>

--- requester/spdm_discovery.cpp
+++ requester/spdm_discovery.cpp
@@ -33,4 +33,10 @@ auto SPDMDiscovery::run() -> sdbusplus::async::task<>
     }
 }
 
+void SPDMDiscovery::remove(const std::string& path)
+{
+    std::erase_if(responderInfos,
+                  [&path](const auto& r) { return r.path == path; });
+}
+
 } // namespace spdm
