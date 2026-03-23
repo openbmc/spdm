e7a43b4f22ab880c826bdb5332a5992a34b59efb
Ratan Gupta <ratankgupta31@gmail.com>
2026-03-23 17:02:47 +0530

    spdmd: Implement the MCTP discovery
    Add MCTP discovery implementation to find SPDM-capable
    devices over D-Bus using coroutine-based async design.
    
    The discovery mechanism queries MCTP endpoints via the
    ObjectMapper and checks for SPDM support by examining
    supported message types.
    
    The discovery implementation allows finding SPDM-capable devices by:
    1. Using mapper to find all MCTP endpoints
    2. Checking for SPDM message type support (0x05)
    3. Gathering device identification info (EID, UUID)
    
    Signed-off-by: Ratan Gupta <ratankgupta31@gmail.com>
    Change-Id: I9034225c3ca88764912f0463ba9f06c4fc4cd299

--- requester/mctp_transport_discovery.cpp
+++ requester/mctp_transport_discovery.cpp
@@ -3,23 +3,67 @@
 
 #include "mctp_transport_discovery.hpp"
 
+#include "utils/mapper.hpp"
+
 #include <phosphor-logging/lg2.hpp>
+#include <sdbusplus/async/proxy.hpp>
 
 #include <algorithm>
+#include <ranges>
+#include <variant>
 
 namespace spdm
 {
+PHOSPHOR_LOG2_USING;
 
-auto MCTPTransportDiscovery::discovery(SPDMDiscovery&)
+auto MCTPTransportDiscovery::discovery(SPDMDiscovery& discovery)
     -> sdbusplus::async::task<>
 {
-    using namespace std::literals;
-    PHOSPHOR_LOG2_USING;
+    static constexpr auto mctpEndpointIntf =
+        "xyz.openbmc_project.MCTP.Endpoint";
+    static constexpr auto uuidIntf = "xyz.openbmc_project.Common.UUID";
+    static constexpr uint8_t spdmMessageType = 0x05;
 
-    // TODO: Not doing any real discovery here but pause for dramatic effect.
-    co_await sdbusplus::async::sleep_for(ctx, 1s);
+    using EndpointVariant = std::variant<uint8_t, std::vector<uint8_t>>;
 
-    debug("MCTPTransportDiscovery: discovery complete");
+    auto instances =
+        co_await mapper::instances::by_interface(ctx, mctpEndpointIntf);
+
+    for (const auto& [path, service] : instances)
+    {
+        auto endpointProxy =
+            sdbusplus::async::proxy().service(service).path(path.str).interface(
+                mctpEndpointIntf);
+
+        auto props =
+            co_await endpointProxy.get_all_properties<EndpointVariant>(ctx);
+
+        auto& messageTypes =
+            std::get<std::vector<uint8_t>>(props.at("SupportedMessageTypes"));
+
+        if (!std::ranges::contains(messageTypes, spdmMessageType))
+        {
+            debug("Endpoint {PATH} does not support SPDM", "PATH", path);
+            continue;
+        }
+
+        auto eid = std::get<uint8_t>(props.at("EID"));
+
+        auto uuid =
+            co_await sdbusplus::async::proxy()
+                .service(service)
+                .path(path.str)
+                .interface(uuidIntf)
+                .get_property<std::string>(ctx, "UUID");
+
+        debug("Found SPDM MCTP device at {PATH}, EID={EID}", "PATH", path,
+              "EID", eid);
+
+        discovery.add(ResponderInfo{path, MctpResponderInfo{eid, uuid},
+                                    TransportType::MCTP});
+    }
+
+    debug("MCTP transport discovery completed");
 }
 
 } // namespace spdm
