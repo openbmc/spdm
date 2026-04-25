04bb70f8f45dc1a6ba737bce10afde74f70a17f1
Patrick Williams <patrick@stwcx.xyz>
2026-04-24 23:20:18 -0400

    mapper: handle exceptions directly
    Move the exception handling into the mapper utility.  There are a few
    expected exceptions:
    
        - If mapper is not present, this is a fatal error.  We can't detect
          this now because sdbusplus doesn't have dedicated types for
          internal errors.  This is coming with another commit.
    
        - get_sub_tree can throw
          xyz.openbmc_project.Common.Error.ResourceNotFound; this
          indicates the mapper result is empty.
    
    Handle these directly in the mapper utility because in almost all cases
    a caller probably just wants an empty array for ResourceNotFound.
    
    Change-Id: Ia9690631a24a1e42f354b820a17d5a7106a2b4b6
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>

--- requester/utils/mapper.cpp
+++ requester/utils/mapper.cpp
@@ -6,9 +6,12 @@
 #include <phosphor-logging/lg2.hpp>
 #include <xyz/openbmc_project/ObjectMapper/client.hpp>
 
+#include <expected>
+
 namespace spdm::mapper
 {
 PHOSPHOR_LOG2_USING;
+using namespace std::string_literals;
 
 namespace instances
 {
@@ -17,11 +20,30 @@ auto by_interface(sdbusplus::async::context& ctx, std::string interface)
 {
     using Mapper = sdbusplus::client::xyz::openbmc_project::ObjectMapper<>;
 
-    auto objects =
-        co_await Mapper(ctx)
-            .service(Mapper::default_service)
-            .path(Mapper::instance_path)
-            .get_sub_tree("/", 0, std::vector<std::string>{interface});
+    auto objects = co_await [&ctx, interface]()
+        -> sdbusplus::async::task<Mapper::get_sub_tree_t::return_type> {
+        try
+        {
+            co_return co_await Mapper(ctx)
+                .service(Mapper::default_service)
+                .path(Mapper::instance_path)
+                .get_sub_tree("/", 0, std::vector<std::string>{interface});
+        }
+        catch (const sdbusplus::internal_exception_t& e)
+        {
+            info("mapper::get_sub_tree failure: {ERROR}", "ERROR", e);
+
+            // TODO: once sdbusplus has the ability to differentiate
+            //       ServiceUnknown we should catch and abort on that one
+            //       instead of checking a string. Mapper being gone is a
+            //       critical issue.
+            if (e.name() == "org.freedesktop.DBus.Error.ServiceUnknown"s)
+            {
+                std::terminate();
+            }
+            co_return {};
+        }
+    }();
 
     instances_t results{};
     for (const auto& [path, services] : objects)
